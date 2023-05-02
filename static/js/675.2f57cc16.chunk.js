f4314ccb1ed58356d9a0ad4d9ca1216aa84a6bab
Miss-Anastasiia <110167985+Miss-Anastasiia@users.noreply.github.com>
2023-05-02 15:07:57 +0000

    Deploying to gh-pages from @ Miss-Anastasiia/goit-react-hw-08-phonebook@14922b937e8bbb5dac8c72ec9cc997065046517f 🚀

---
+++ static/js/675.2f57cc16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[675],{9675:function(e,t,o){o.r(t);var r=o(4554),n=o(890),c=o(184);t.default=function(){return(0,c.jsx)(r.Z,{sx:{textAlign:"center",bgcolor:"background.main",color:"text.primary",p:3},children:(0,c.jsx)(n.Z,{variant:"h5",component:"h2",color:"inherit",sx:{flexGrow:1},children:"Welcome to the Phonebook"})})}}}]);
+//# sourceMappingURL=675.2f57cc16.chunk.js.map
